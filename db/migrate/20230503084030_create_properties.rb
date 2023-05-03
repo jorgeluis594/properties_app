8d11338a0d25bcc3d0ce4a9e8789ce1cc58d2d3f
Jorge Gonzalez <lgonzalez@truora.com>
2023-05-03 03:53:06 -0500

    Add Property model

---
+++ db/migrate/20230503084030_create_properties.rb
@@ -0,0 +1,14 @@
+class CreateProperties < ActiveRecord::Migration[7.0]
+  def change
+    create_table :properties do |t|
+      t.integer :type, default: 0
+      t.monetize :price, null: false
+      t.string :region, null: false
+      t.string :locality, null: false
+      t.integer :bedrooms
+      t.decimal :bathrooms, precision: 4, scale: 2
+
+      t.timestamps
+    end
+  end
+end
