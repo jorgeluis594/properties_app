412c1f06d4c6df90e40557182749fc026281dc66
Jorge Gonzalez <lgonzalez@truora.com>
2023-05-03 04:26:29 -0500

    Add validations of property model

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,25 @@
 class Property < ApplicationRecord
+  has_many_attached :images
+
+  enum :type, %i[rent sale]
+
+  monetize :price_cents
+
+  validates :price_cents, numericality: { greater_than: 0 }
+  validates :area, numericality: { greater_than: 0 }
+  validates :bedrooms, numericality: { greater_than: 0 }
+  validates :bathrooms, numericality: { greater_than: 0 }
+  validates :region, :locality, :area, presence: true
+
+  validate :count_of_images
+
+  private
+
+  MINIMUM_IMAGES_NUMBER = 3
+
+  def count_of_images
+    unless images.length >= MINIMUM_IMAGES_NUMBER
+      errors.add(:images, "should be greater or equal than 3")
+    end
+  end
 end
