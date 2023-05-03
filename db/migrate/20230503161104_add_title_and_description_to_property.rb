acc3a5728bb6607040e68448f041824660f48096
Jorge Gonzalez <lgonzalez@truora.com>
2023-05-03 11:46:50 -0500

    Add title and description to Property model

---
+++ db/migrate/20230503161104_add_title_and_description_to_property.rb
@@ -0,0 +1,6 @@
+class AddTitleAndDescriptionToProperty < ActiveRecord::Migration[7.0]
+  def change
+    add_column :properties, :title, :string
+    add_column :properties, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_03_085715) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_03_161104) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -50,6 +50,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_085715) do
     t.decimal "area", precision: 7, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "description"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
