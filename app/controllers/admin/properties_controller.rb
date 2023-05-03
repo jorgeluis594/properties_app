d91f74afbe7d0b8821b905640ab14c5479ad2717
Jorge Gonzalez <lgonzalez@truora.com>
2023-05-03 06:23:28 -0500

    Add create action to properties controller

--- app/controllers/admin/properties_controller.rb
+++ app/controllers/admin/properties_controller.rb
@@ -6,7 +6,33 @@ class Admin::PropertiesController < ApplicationController
   def index
   end
 
+  def create
+    if property.save
+      redirect_to admin_properties_path
+    else
+      flash[
+        :error
+      ] = "Error creating property: #{property.errors.full_messages.to_sentence}"
+      redirect_to new_admin_property_path
+    end
+  end
+
   def show
     @property = Property.find(params[:id])
+  end
+
+  private
+
+  def property_params
+    params.require(:property).permit(
+      :property_type,
+      :price_cents,
+      :area,
+      :bedrooms,
+      :bathrooms,
+      :region,
+      :locality,
+      images: []
+    )
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def alert_class_for_type(type)
+    case type
+    when "error"
+      "danger"
+    else
+      type
+    end
+  end
 end
